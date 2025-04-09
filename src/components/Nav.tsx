b9aee938cae7cacd98ac14ff22dc383143c9e2b2
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-04-10 00:25:51 +0300

    Center navigation links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,11 @@
 export default function Nav() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>HOME</li>
-          <li>BLOG</li>
-          <li>CONTACT</li>
-        </ul>
-      </nav>
+      <ul>
+        <li>HOME</li>
+        <li>BLOG</li>
+        <li>CONTACT</li>
+      </ul>
     </>
   );
 }
