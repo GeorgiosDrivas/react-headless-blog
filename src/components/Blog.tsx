e51f6f0ee548f8b386954e57e1e9bb3bb41883f9
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-04-15 18:40:20 +0300

    Display post titles in blog general page

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,17 +1,37 @@
 import { useEffect, useState } from "react";
-import { apiPath } from "../../variables";
+import { apiCategoriesPath, apiPostsPath } from "../../variables";
 import { CategoryType } from "../types/blog-page-types";
 import { Link } from "react-router-dom";
 
+type PostsType = {
+  id: number;
+  title: { rendered: string };
+};
+
 export default function Blog() {
   const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [posts, setPosts] = useState<PostsType[]>([]);
 
   useEffect(() => {
-    fetch(apiPath)
-      .then((response) => response.json())
-      .then((data) => {
-        setCategories(data);
-      });
+    const getCategories = async () => {
+      fetch(apiCategoriesPath)
+        .then((response) => response.json())
+        .then((data) => {
+          setCategories(data);
+        });
+    };
+
+    const getPosts = async () => {
+      fetch(apiPostsPath)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setPosts(data);
+        });
+    };
+
+    getCategories();
+    getPosts();
   }, []);
 
   return (
@@ -25,6 +45,13 @@ export default function Blog() {
           >
             <p>{category.name}</p>
           </Link>
+        ))}
+      </div>
+      <div id="posts">
+        {posts.map((post: PostsType) => (
+          <div key={post.id} className="single-post">
+            <p>{post.title.rendered}</p>
+          </div>
         ))}
       </div>
     </>
--- variables.ts
+++ variables.ts
@@ -1,1 +1,2 @@
-export const apiPath = "http://headless-blog.local/wp-json/wp/v2/categories";
+export const apiCategoriesPath = "http://headless-blog.local/wp-json/wp/v2/categories";
+export const apiPostsPath = "http://headless-blog.local/wp-json/wp/v2/posts";
