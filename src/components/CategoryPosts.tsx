b139cfdd93e470e7a251fdf8fd7ea875140d6957
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-04-18 20:25:48 +0300

    Add a container in single category and back link

--- src/components/CategoryPosts.tsx
+++ src/components/CategoryPosts.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { PostsType } from "../types/posts-types";
 import { apiPostsPath } from "../../variables";
@@ -31,30 +31,35 @@ const CategoryPosts = () => {
   }, [category]);
 
   return (
-    <div className="posts">
-      {posts.map((post: PostsType) => (
-        <div key={post.id} className="single-post">
-          <div className="post-img">
-            {post._embedded?.["wp:featuredmedia"]?.[0]?.source_url && (
-              <img
-                src={post._embedded["wp:featuredmedia"][0].source_url}
-                alt={post.title.rendered}
+    <div className="container">
+      <Link className="back-link" to={"/blog"}>
+        Back
+      </Link>
+      <div className="posts">
+        {posts.map((post: PostsType) => (
+          <div key={post.id} className="single-post">
+            <div className="post-img">
+              {post._embedded?.["wp:featuredmedia"]?.[0]?.source_url && (
+                <img
+                  src={post._embedded["wp:featuredmedia"][0].source_url}
+                  alt={post.title.rendered}
+                />
+              )}
+            </div>
+            <div className="post-details">
+              <a href={post.link} className="post-title">
+                {post.title.rendered}
+              </a>
+              <div
+                className="post-excerpt"
+                dangerouslySetInnerHTML={{
+                  __html: post.excerpt.rendered.slice(0, 150),
+                }}
               />
-            )}
+            </div>
           </div>
-          <div className="post-details">
-            <a href={post.link} className="post-title">
-              {post.title.rendered}
-            </a>
-            <div
-              className="post-excerpt"
-              dangerouslySetInnerHTML={{
-                __html: post.excerpt.rendered.slice(0, 150),
-              }}
-            />
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
