acb920f76eb90aff9bdf6a6da655d4f80d641ec4
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-04-24 15:29:39 +0300

    About page styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,46 @@
 export default function About() {
   return (
     <>
-      <div className="container">
-        <h1>About this project</h1>
-        <p>
-          This project is a simple blog that uses WordPress as a headless CMS
-          and React and SASS for the frontend.
-          <br />
-        </p>
-        <h2>Tech Stach</h2>
-        <ul>
-          <li>WordPress API</li>
-          <li>React</li>
-          <li>TypeScript</li>
-          <li>SASS</li>
-          <li>Vite</li>
-          <li>React Router</li>
-        </ul>
-        <h2>Resources</h2>
-        <div id="contact_resources">
-          <a
-            href="https://github.com/GeorgiosDrivas/react-headless-blog"
-            target="_blank"
-          >
-            Github repository link
-          </a>
-          <a href="https://www.linkedin.com/in/drivasgeorgios/" target="_blank">
-            LinkedIn Profile
-          </a>
-          <a href="https://www.georgiosdrivas.eu/" target="_blank">
-            Portfolio website
-          </a>
-        </div>
+      <div className="container" id="contact">
+        <section>
+          <h2>About this project</h2>
+          <p>
+            This project is a simple blog that uses WordPress as a headless CMS
+            and React and SASS for the frontend.
+            <br />
+          </p>
+        </section>
+        <section>
+          <h2>Tech Stach</h2>
+          <ul>
+            <li>WordPress API</li>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>SASS</li>
+            <li>Vite</li>
+            <li>React Router</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Resources</h2>
+          <div id="contact_resources">
+            <a
+              href="https://github.com/GeorgiosDrivas/react-headless-blog"
+              target="_blank"
+            >
+              Github repository link
+            </a>
+            <a
+              href="https://www.linkedin.com/in/drivasgeorgios/"
+              target="_blank"
+            >
+              LinkedIn Profile
+            </a>
+            <a href="https://www.georgiosdrivas.eu/" target="_blank">
+              Portfolio website
+            </a>
+          </div>
+        </section>
       </div>
     </>
   );
